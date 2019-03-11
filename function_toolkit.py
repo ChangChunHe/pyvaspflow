65bbd781104ef4be51c668028cd0a9f2b41bfb0b
ChangChunHe <731176792@qq.com>
2019-03-11 19:35:29 +0800

    wrap write_vasp

--- function_toolkit.py
+++ function_toolkit.py
@@ -1,4 +1,8 @@
 import numpy as np
+from sagar.io.vasp import  write_vasp
+from os import path
+
+
 def generate_all_basis(N1,N2,N3):
     n1,n2,n3 = 2*N1+1, 2*N2+1, 2*N3+1
     x = np.tile(np.arange(-N3,N3+1),n1*n2)
@@ -14,7 +18,7 @@ def refine_points(tetra,extend_S,C):
     tetra_cen = np.zeros((n,3))
     for ii in range(n):
         tetra_cen[ii] = np.mean(extend_S[tetra[ii]],axis=0)
-    tetra_cen = [cen  for cen in tetra_cen if min(np.linalg.norm(cen-extend_S,axis=1))>1.5]
+    tetra_cen = [cen for cen in tetra_cen if min(np.linalg.norm(cen-extend_S,axis=1))>1.5]
     final_res = []
     for cen in tetra_cen:
         d = np.linalg.norm(cen-tetra_cen,axis=1)
@@ -27,3 +31,10 @@ def refine_points(tetra,extend_S,C):
     idx = np.sum((final_res <= 0.99) &(final_res >= 0.01),axis=1)
     idx = np.where(idx == 3)[0]
     return final_res[idx]
+
+
+def wirite_poscar(cell,purity_atom,folder,idx):
+    comment = 'POSCAR-' + purity_atom + '-defect'
+    filename = '{:s}_id{:d}'.format(comment, idx)
+    file = path.join('./'+folder, filename)
+    write_vasp(cell,file)
