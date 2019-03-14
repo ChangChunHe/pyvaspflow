684881351d3c1a8e84d915be190c1932d53249ed
ChangChunHe <731176792@qq.com>
2019-03-14 10:15:59 +0800

    rewrite some fuc using shell command

--- defect_cal.py
+++ defect_cal.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+
 import linecache as lc
 import numpy as np
 import os
 import subprocess
-#%%class
-class Acquire_value():
-    def __init__(self,file_path='./',atomic_num=3):
 
-        self.file = file_path
+
+class Acquire_value():
+    def __init__(self,data_folder='./',atomic_num=3):
+
+        self.data_folder = data_folder
         self.atomic_num = atomic_num
 
     def _get_line(self,file_tmp,rematch=None):
@@ -14,44 +19,33 @@ class Acquire_value():
         return [int(ii) - 1 for ii in subprocess.check_output(['cut','-d',':','-f','1'],stdin=grep_res.stdout).decode('utf-8').split()]
 
     def get_energy(self):
-
-        file_out = self.file + 'OUTCAR'
-
-        tmp_line = self._get_line(file_out,rematch='TOTEN')
-        toten = float(lc.getlines(file_out)[tmp_line[-1]].split()[4])
-        return toten
+        file_outcar = os.path.join(self.data_folder,'OUTCAR')
+        grep_res = subprocess.Popen(['grep','TOTEN',file_outcar],stdout=subprocess.PIPE)
+        return float(subprocess.check_output(['tail','-1'],stdin=grep_res.stdout).decode('utf-8').split()[-2])
 
     def get_fermi(self):
+        # read from scf calculation
+        file_outcar = os.path.join(self.data_folder,'OUTCAR')
+        grep_res = subprocess.Popen(['grep','E-fermi',file_outcar],stdout=subprocess.PIPE)
+        return float(subprocess.check_output(['tail','-1'],stdin=grep_res.stdout).decode('utf-8').split()[2])
 
-        file_out = self.file + 'OUTCAR'
+    def get_Ne_defect_free(self):
+        # you can get valance electrons in your defect-free system
+        file_pos = os.path.join(self.data_folder, 'POSCAR')
+        file_pot = os.path.join(self.data_folder, 'POTCAR')
+        ele_num_atom = [float(i) for i in  subprocess.run(['grep','ZVAL',file_pot],stdout=subprocess.PIPE).stdout.decode('utf-8').split()[5::9]]
+        atom_num = [float(i) for i in lc.getlines(file_pos)[6].split()]
+        return np.dot(atom_num,ele_num_atom)
 
-        tmp_line = self._get_line(file_out,rematch='E-fermi')
-        fermi = float(lc.getlines(file_out)[tmp_line[0]].split()[2]) # read the first or last?
-        return fermi
-
-    def get_Ne_p(self):
-        # get valance electrons
-        file_pos = self.file + 'POSCAR'
-        file_pot =  self.file + 'POTCAR'
-        tmp_pos = lc.getlines(file_pos)[6].split()
-        tmp_lines = self._get_line(file_pot,rematch='ZVAL')
-        nelect_p = 0
-        for ii in range(len(tmp_lines)):
-            tmp_zval = lc.getlines(file_pot)[tmp_lines[ii]].split()[5]
-            nelect_p+=round(int(tmp_pos[ii])*float(tmp_zval))
-        return nelect_p
-
-    def get_Ne_d(self):
-        #get electrons number in OUTCAR
-        file_out = self.file + 'OUTCAR'
-
-        tmp_line = self._get_line(file_out,rematch='NELECT')
-        nelect_d = lc.getlines(file_out)[tmp_line[0]].split()[2]
-        return round(float(nelect_d))
+    def get_Ne_defect(self):
+        #get all valance electrons number in OUTCAR
+        file_outcar = os.path.join(self.data_folder,'OUTCAR')
+        grep_res = subprocess.run(['grep','NELECT',file_outcar],stdout=subprocess.PIPE)
+        return int(float(grep_res.stdout.decode('utf-8').split()[2]))
 
     def get_image(self):
 
-        file_image = self.file + 'image_cor/OUTCAR'
+        file_image = self.data_folder + 'image_cor/OUTCAR'
 
         tmp_line = self._get_line(file_image,rematch='Ewald')
         Ewald = lc.getlines(file_image)[tmp_line[0]].split()[4]
@@ -59,8 +53,8 @@ class Acquire_value():
 
     def get_PA(self):
         # get potential alignment correlation
-        file_PA = self.file + 'OUTCAR'
-        file_pos = self.file + 'POSCAR'
+        file_PA = self.data_folder + 'OUTCAR'
+        file_pos = self.data_folder + 'POSCAR'
 
         tmp_atom_line = round(sum(map(int,lc.getlines(file_pos)[6].split()))/5)+3
         tmp_match_line = self._get_line(file_PA,rematch='electrostatic')
@@ -73,27 +67,24 @@ class Acquire_value():
                     return line.split()[self.atomic_num %5*2-1]
 
     def get_gap(self):
-
-        file_gap = self.file + 'EIGENVAL'
-
-        nelect_vbm = int(lc.getlines(file_gap)[5].split()[0])//2
-        nelect_cbm = nelect_vbm+1
-
-        tmp_lines = self._get_line(file_gap,rematch=' '+str(nelect_vbm)+' ')
-        Evbms = []
-        for ii in tmp_lines:
-            Evbms.append(lc.getlines(file_gap)[ii].split()[1])
-
-        tmp_lines = self._get_line(file_gap,rematch=' '+str(nelect_cbm)+' ')
-        Ecbms = []
-        for ii in tmp_lines:
-            Ecbms.append(lc.getlines(file_gap)[ii].split()[1])
-
-        Ecbm, Evbm =  min(map(float,Ecbms)), max(map(float,Evbms))
-        return Ecbm, Evbm, Ecbm - Evbm
+        file_eig = os.path.join(self.data_folder,'EIGENVAL')
+        line6 = np.genfromtxt(file_eig,skip_header=5,max_rows=1)
+        kpt_num, eig_num = int(line6[1]), int(line6[2])
+        all_eigval = np.zeros((eig_num, kpt_num*2))
+        for ii in range(kpt_num):
+             all_eigval[:,2*ii:2*ii+2] = np.genfromtxt(file_eig,skip_header=8+eig_num*int(ii)+2*ii,\
+                                       max_rows=eig_num,usecols=(1,2))
+        elec_num =  np.mean(all_eigval[:,1::2],axis=1)
+        idx1 = np.where(elec_num > 0.9)
+        idx2 = np.where(elec_num < 0.5)
+        if idx1[0][-1] - idx2[0][0] == -1:
+            vbm = np.max(all_eigval[idx1[0][-1],::2])
+            cbm = np.min(all_eigval[idx2[0][0],::2])
+            gap = cbm - vbm
+        return cbm,vbm,gap
 
     def get_miu(self):
-        file_value, file_out = self.file + 'value', self.file + 'out'
+        file_value, file_out = self.data_folder + 'value', self.data_folder + 'out'
         with open (file_value,'r') as f:
             miu_i = {}
             for line in f.readlines():
@@ -120,25 +111,25 @@ files = ['N_Si','Vac_Si_defect','P_Si']
 def main_Hf(files, atomic_num=None, epsilon=None):
     diff_d = {}
 
-    for file in files:
+    for data_folder in files:
         SC_energy = Acquire_value('scf/').get_energy()
-        nelect_p = Acquire_value(file+'/').get_Ne_p()
+        nelect_p = Acquire_value(data_folder+'/').get_Ne_p()
         Ecbm, Evbm, gap = Acquire_value('scf/').get_gap()
-        miu = Acquire_value(file+'/').get_miu()
+        miu = Acquire_value(data_folder+'/').get_miu()
 
         dirs = []
-        for ii in os.listdir(file+'/'):
+        for ii in os.listdir(data_folder+'/'):
             if 'NELECT' in ii:
                 dirs.append(ii)
         for path in dirs:
-            nelect_d = Acquire_value(file+'/'+path+'/').get_Ne_d()
+            nelect_d = Acquire_value(data_folder+'/'+path+'/').get_Ne_d()
             charge_state = nelect_p - nelect_d
-            D_energy = Acquire_value(file+'/'+path+'/scf/').get_energy()
+            D_energy = Acquire_value(data_folder+'/'+path+'/scf/').get_energy()
 
             if atomic_num is None:
                 V_delt = 0
             else:
-                V_delt = Acquire_value(file+'/'+path+'/scf/', atomic_num=atomic_num).get_PA() -\
+                V_delt = Acquire_value(data_folder+'/'+path+'/scf/', atomic_num=atomic_num).get_PA() -\
                             Acquire_value('scf/', atomic_num=atomic_num).get_PA()
 
 
@@ -152,11 +143,10 @@ def main_Hf(files, atomic_num=None, epsilon=None):
             Hf_vbm = D_energy + E_imagecor - SC_energy + miu + charge_state * (Evbm-0.5+V_delt)
             Hf_cbm = D_energy + E_imagecor - SC_energy + miu + charge_state * (Evbm+gap+0.5+V_delt)
             for ii in range(len(miu)):
-                diff_d[file] = diff_d.get(file,[])
-                diff_d[file].append([charge_state, D_energy, SC_energy, V_delt, E_imagecor, miu[ii], gap, Hf_vbm[ii], Hf_cbm[ii]])
-
+                diff_d[data_folder] = diff_d.get(data_folder,[])
+                diff_d[data_folder].append([charge_state, D_energy, SC_energy, V_delt, E_imagecor, miu[ii], gap, Hf_vbm[ii], Hf_cbm[ii]])
             return diff_d
+
 if __name__ == "__main__":
-    Av = Acquire_value()
-    count = Av._get_line(file_tmp='defect_maker.py',rematch='def')
-    print(count)
+    Av = Acquire_value('./BN-plane')
+    print(Av.get_Ne_p())
