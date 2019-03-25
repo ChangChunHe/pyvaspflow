a11b5448ec9d7995e4b69bdb09145bc2248741ad
ChangChunHe <731176792@qq.com>
2019-03-25 14:15:26 +0800

    merge defecemaker to pyvasp

--- pyvasp.py
+++ pyvasp.py
@@ -8,6 +8,7 @@ import function_toolkit as ft
 import linecache as lc
 from sagar.io.vasp import read_vasp, write_vasp
 import os
+from  defect_maker import DefectMaker
 
 
 @click.group()
@@ -146,6 +147,26 @@ def cell(pcell_filename, volume):
     write_vasp(supcell, 'supcell')
 
 
+
+@cli.command('get_purity',short_help="Get purity POSCAR")
+@click.argument('poscar', metavar='<primitive_cell_file>',
+                type=click.Path(exists=True, resolve_path=True, readable=True, file_okay=True))
+@click.option('--purity_in','-i', default='Vacc', type=str)
+@click.option('--purity_out','-o', default='all', type=str)
+@click.option('--symprec','-s', default='1e-3', type=float)
+def get_purity_poscar(poscar, purity_in, purity_out,symprec):
+    DM = DefectMaker(no_defect=poscar)
+    DM.get_purity_defect(purity_out=purity_out,purity_in=purity_in,symprec=symprec)
+
+
+@cli.command('get_tetrahedral',short_help="Get get tetrahedral sites of POSCAR")
+@click.argument('poscar', metavar='<primitive_cell_file>',
+                type=click.Path(exists=True, resolve_path=True, readable=True, file_okay=True))
+@click.option('--purity_in','-i', default='H', type=str)
+@click.option('--isunique','-u', default=True, type=bool)
+def get_tetrahedral_poscar(poscar,purity_in,isunique):
+    DM = DefectMaker(no_defect=poscar)
+    DM.get_tetrahedral_defect(isunique=isunique,purity_in=purity_in)
 
 if __name__ == "__main__":
     cli()
