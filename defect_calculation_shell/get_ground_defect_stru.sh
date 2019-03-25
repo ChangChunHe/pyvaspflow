8ec2d3dacb67d4446844852ef76d14efa18de4ff
ChangChunHe <731176792@qq.com>
2019-03-25 15:08:19 +0800

    add input parameter

--- defect_calculation_shell/get_ground_defect_stru.sh
+++ defect_calculation_shell/get_ground_defect_stru.sh
@@ -6,22 +6,36 @@ elif [ ! -n "$vasp_version" ]
 then    vasp_version="vasp_std"
 fi
 
-for ii in `ls`
-do
-if [[ $ii == *"POSCAR"*  ]]
+
+
+
+
+# calculate the energy of all strcutures
+folder=$1
+if [ -z $folder ]
 then
-a=$ii
-mkdir $ii-cal
-cp $ii $ii-cal/POSCAR
-cd $ii-cal
-potcar.sh
-kpoints.sh
-incar.sh
-stru_relax.sh
-echo $ii `tail -1 OSZICAR |awk '{print $5}'`  >> energy_out
-cd ..
+echo You should input a directory
+exit
 fi
+IFS='-' read -ra A <<< "$folder"
+atom_in=${A[1]}
+
+cd $folder
+
+for pos in `ls`
+do
+mkdir ${pos}-dir
+mv $pos ${pos}-dir/POSCAR
+cd ${pos}-dir
+stru_optimization.sh
+echo $pos `tail -1 OSZICAR |awk '{print $5}'` >> ../energy_out
+cd ..
 done
-ground_stru_num=`cat energy_out|sort -nk2r|head -1|awk '{print $1}'`
-b=`echo $a |cut -d'_' -f 1`
-mv ${b}_idx${ground_stru_num}.vasp POSCAR_ground_stru # this is the ground state
+# gd is the ground structure
+gd=`sort -k2n energy_out|awk 'NR==1{print $1}'`
+
+# calculate scf of the ground structure
+cp -r  $gd-dir $atom_in-defect
+cd $atom_in-defect
+stru_scf.sh
+cd ../..
