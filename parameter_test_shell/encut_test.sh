3c16cbd6a3fb90ff572e5d703a6eaf0e76daa961
ChangChunHe <731176792@qq.com>
2019-03-30 09:56:41 +0800

    rearange bound

--- parameter_test_shell/encut_test.sh
+++ parameter_test_shell/encut_test.sh
@@ -4,8 +4,8 @@ module load vasp/5.4.4-impi-mkl
 
 if [  -f  POTCAR ]
 then
-max_encut=`grep ENMAX POTCAR |sort -k3nr|awk 'NR==1{print $3*1.4}' `
-min_encut=`grep ENMAX POTCAR |sort -k3nr|awk 'NR==1{print $3*0.9}' `
+max_encut=`grep ENMAX POTCAR |sort -k3nr|awk 'NR==1{print $3*1.6}' `
+min_encut=`grep ENMAX POTCAR |sort -k3nr|awk 'NR==1{print $3*0.8}' `
 else
 echo No POTCAR found
 fi
@@ -18,4 +18,3 @@ sed -i -e '/NSW/c NSW=0'  -e "/ENCUT/c  ENCUT=$val" INCAR
 stru_one_step.sh
 echo $val `awk '/TOTEN/{print $(NF-1)}' OUTCAR |tail -1` `awk '/CPU/{print $NF}' OUTCAR` >> encut_energy.out
 done
-
