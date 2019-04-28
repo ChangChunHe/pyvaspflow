4a131e9d35a5cfbf6150f8896fd7f2f5910d28ab
ChangChunHe <731176792@qq.com>
2019-04-28 17:20:36 +0800

    add clean parse func

--- pydefcal/run_vasp/runvasp.py
+++ pydefcal/run_vasp/runvasp.py
@@ -6,33 +6,39 @@ from pydefcal.utils import run,read_json
 from os import path
 import json
 
-def is_inqueue(jobid):
-    res = run('squeue','grep '+str(jobid)).std_out_err
+
+def is_inqueue(job_id):
+    res = run('squeue','grep '+str(job_id)).std_out_err
     if res[0] == '':
         return False
     else:
         return True
 
-def submit_job(node_name, cpu_num,node_num=1,jobname='job-name'):
-    write_job_file(node_name,cpu_num,node_num,jobname)
+def submit_job(node_name, cpu_num,node_num=1,job_name='job-name'):
+    write_job_file(node_name,cpu_num,node_num,job_name)
     res = run('sbatch job.sh')
     std = res.std_out_err
     return std[0].split()[-1],std[1]
 
 
-def job_status(jobid):
-    res = run('squeue','grep '+str(jobid)).std_out_err
+def job_status(job_id):
+    res = run('squeue','grep '+str(job_id)).std_out_err
     stdout = res[0].split()
     if stdout == [] :
-        print('Not found jobid in queue')
+        print('Not found job_id in queue')
         return  None
-    return dict(zip(['jobid','part','name','user','status','time','node','nodelist'],stdout))
+    return dict(zip(['job_id','part','name','user','status','time','node','nodelist'],stdout))
 
-def write_job_file(node_name,cpu_num,node_num,jobname):
+def write_job_file(node_name,cpu_num,node_num,job_name):
     json_f = read_json()
     with open('job.sh','w') as f:
         f.writelines('#!/bin/bash -l\n')
-        f.writelines('#SBATCH -J '+jobname+'\n')
+        f.writelines('#SBATCH -J '+job_name+'\n')
         f.writelines('#SBATCH -p '+node_name+' -N '+ str(node_num) +' -n '+str(cpu_num)+'\n\n')
         f.writelines(json_f['job']['prepend']+'\n')
         f.writelines(json_f['job']['exec']+'\n')
+
+def clean_parse(kw,key,def_val):
+    val = kw.get(key,def_val)
+    kw.pop(key,None)
+    return val,kw
