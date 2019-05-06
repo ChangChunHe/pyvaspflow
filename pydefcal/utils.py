7906231aeac9ccc07207fa03834820e6ab5f16b2
ChangChunHe <731176792@qq.com>
2019-05-06 10:05:40 +0800

    rm the same get_all_basis

--- pydefcal/utils.py
+++ pydefcal/utils.py
@@ -4,15 +4,6 @@ from os import path
 from sagar.element.base import periodic_table_dict as ptd
 import json
 
-def generate_all_basis(N1,N2,N3):
-    n1,n2,n3 = 2*N1+1, 2*N2+1, 2*N3+1
-    x = np.tile(np.arange(-N3,N3+1),n1*n2)
-    y = np.tile(np.repeat(np.arange(-N2,N2+1),n3),n1)
-    z = np.repeat(np.arange(-N1,N1+1),n2*n3)
-    x,y,z = np.reshape(x,(-1,1)),np.reshape(y,(-1,1)),np.reshape(z,(-1,1))
-    tmp = np.hstack((z,y))
-    return np.hstack((tmp,x))
-
 
 def refine_points(tetra,extend_S,C,min_d=1):
     n = np.shape(tetra)[0]
