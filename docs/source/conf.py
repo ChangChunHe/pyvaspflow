b0e06a9a2e8c7375b4547fc0f43c46c30f824040
ChangChunHe <731176792@qq.com>
2019-05-27 13:21:51 +0800

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -144,7 +144,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'sagar', 'sagar Documentation',
+    (master_doc, 'pyvaspflow', 'pyvaspflow Documentation',
      [author], 1)
 ]
 
