7d311606c496f49b05a00e78daafb4d442ffdadc
ChangChunHe <731176792@qq.com>
2019-06-14 14:47:50 +0800

    change url of pyvasp

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name = "pyvaspflow",
     #recursively.)
     packages = ['pyvaspflow','pyvaspflow.io','pyvaspflow.vasp','pyvaspflow.defect_cal'],
     install_requires=['numpy','sagar','seekpath','matplotlib'],
-    url="https://github.com/ChangChunHe/Defect-Formation-Calculation",
+    url="https://github.com/ChangChunHe/pyvaspflow",
     entry_points={
         'console_scripts': [
         'pyvasp = pyvaspflow.pyvasp:cli',
